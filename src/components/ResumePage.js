a5ce893a3f452eab7f3f2a50c4042b0012a570be
ScTigerFang <sctigerfang69@gmail.com>
2024-04-10 16:33:43 -0500

    updating resume

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -1,61 +1,63 @@
 import React from "react";
-import "../Styling/ResumePage.css"; // Assuming you create a specific CSS file for styling
+import "../Styling/ResumePage.css";
+import {
+  personalInfo,
+  professionalOverview,
+  skills,
+  experiences,
+  education,
+} from "../data/resumeData"; // Adjust the path as necessary
 
 function ResumePage() {
   return (
     <div className="resume-container">
       <header className="resume-header">
-        <h1>Daniel Beals</h1>
-        <p>Email: danielbealsprofemail@gmail.com | Phone: 641-629-9635</p>
+        <h1>{personalInfo.name}</h1>
         <p>
-          LinkedIn: Daniel Beals, Pella Iowa Link
-          https://www.linkedin.com/in/daniel-beals-470682155/
+          Email: {personalInfo.email} | Phone: {personalInfo.phone}
+        </p>
+        <p>
+          LinkedIn:{" "}
+          <a href={personalInfo.linkedIn}>
+            {personalInfo.name} {personalInfo.location}
+          </a>
         </p>
       </header>
 
       <section className="professional-overview">
         <h2>Professional Overview</h2>
-        <p>
-          A dedicated and innovative Full Stack Developer with extensive
-          experience in designing, developing, and maintaining responsive and
-          efficient software solutions. Proficient in both front-end and
-          back-end technologies, I have a proven track record of leveraging the
-          full software development lifecycle to deliver complex projects from
-          conception through deployment. With a strong foundation in React, C#,
-          .NET Core, APIs, SQL, and modern cloud infrastructure, I excel at
-          building scalable applications that meet and exceed business
-          requirements. My approach combines technical excellence with a deep
-          commitment to understanding user needs, resulting in software that not
-          only performs exceptional but also delivers a seamless user
-          experience. A natural problem-solver and team collaborator, I thrive
-          in environments where I can contribute to innovative solutions and
-          continuous improvement. I naturally lean towards being a mentor as I
-          show a strong skill set for leadership and helping others.
-        </p>
+        <p>{professionalOverview}</p>
       </section>
 
       <section className="skills">
         <h2>Skills</h2>
-        <ul>
-          {/* List your skills */}
-          <li>Entity Framework</li>
-          <li>C#</li>
-          {/* Add the rest of the skills */}
+        <ul className="skills-list">
+          {skills.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
         </ul>
       </section>
 
       <section className="experience">
         <h2>Experience</h2>
-        {experiences.map((exp) => (
-          <article key={exp.company}>
+        {experiences.map((exp, index) => (
+          <article key={index}>
             <h3>
               {exp.role} - {exp.company}
             </h3>
             <p>{exp.duration}</p>
+            <div className="experience-skills">
+              <h4>Skills Utilized</h4>
+              <ul className="experience-skills-list">
+                {exp.skills.map((skill, sIndex) => (
+                  <li key={sIndex}>{skill}</li>
+                ))}
+              </ul>
+            </div>
             <p>{exp.overview}</p>
             <ul>
-              {exp.contributions.map((contribution, index) => (
-                <li key={index}>{contribution}</li>
+              {exp.contributions.map((contribution, cIndex) => (
+                <li key={cIndex}>{contribution}</li>
               ))}
             </ul>
           </article>
@@ -64,48 +66,18 @@ function ResumePage() {
 
       <section className="education">
         <h2>Education</h2>
-        <p>
-          Indian Hills Community College - Associate's degree, Computer Science,
-          2016 - 2018
-        </p>
+        {education.map((edu, index) => (
+          <p key={index}>
+            {edu.school} - {edu.degree}, {edu.duration}
+          </p>
+        ))}
       </section>
 
-      {/* Include other sections like Licenses & Certifications, Honors & Awards as needed */}
-
       <footer className="resume-footer">
-        {/* Footer can include date of last update or any other info */}
-        <p>Last updated on: {/* Dynamic date or static */}</p>
+        <p>Last updated on: {new Date().toLocaleDateString()}</p>
       </footer>
     </div>
   );
 }
 
 export default ResumePage;
-
-// You might define this outside your component or in a separate file for clarity
-const experiences = [
-  {
-    role: "Software Engineer II",
-    company: "Medical Solutions",
-    duration: "Mar 2023 - Mar 2024",
-    overview:
-      "Project Overview: Contributed to the development and maintenance of a crucial system designed for processing and converting hospital timesheets into formats compatible with internal systems. This initiative ensured seamless data integration and enhanced operational efficiency.",
-    contributions: [
-      "Collaborative Backend Overhaul: Actively participated in the collaborative effort...",
-      "Frontend Revitalization: Led the overhaul of the frontend to align with the new backend architecture.",
-      // Add more contributions as needed
-    ],
-  },
-  {
-    role: "Software Engineer II",
-    company: "DLL",
-    duration: "Sep 2020 - Nov 2022",
-    overview:
-      "Filled in for absent Scrum Master, overseeing 2 teams, and 4 projects. Mentored 2 interns, contributing to their development and integration within the team. Led a project on contracts tracking with a tech stack of React JS/JavaScript/Node with C# API backend using Azure SQL Database. Managed and enhanced the Express Finance Portal project, migrating from AWS to Azure with significant data migration. Supported the development of a customer-initiated credit application generating substantial revenue.",
-    contributions: [
-      "Mentored 2 interns, contributing to their development and integration within the team.",
-      // Add more contributions as needed
-    ],
-  },
-  // Add more experiences as needed
-];
--- src/data/resumeData.js
+++ src/data/resumeData.js
@@ -2,15 +2,41 @@ export const personalInfo = {
   name: "Daniel Beals",
   email: "danielbealsprofemail@gmail.com",
   phone: "641-629-9635",
-  linkedIn: "Daniel Beals, Pella Iowa",
+  location: "Pella, Iowa",
+  linkedIn: "https://www.linkedin.com/in/daniel-beals-470682155/",
 };
 
-export const professionalOverview = `A dedicated and innovative Full Stack Developer...`;
+export const professionalOverview = `A dedicated and innovative Full Stack Developer with extensive experience in designing, developing, and maintaining responsive and efficient software solutions. Proficient in both front-end and back-end technologies, I have a proven track record of leveraging the full software development lifecycle to deliver complex projects from conception through deployment. With a strong foundation in React, C#, .NET Core, APIs, SQL, and modern cloud infrastructure, I excel at building scalable applications that meet and exceed business requirements. My approach combines technical excellence with a deep commitment to understanding user needs, resulting in software that not only performs exceptional but also delivers a seamless user experience. A natural problem-solver and team collaborator, I thrive in environments where I can contribute to innovative solutions and continuous improvement. I naturally lean towards being a mentor as I show a strong skill set for leadership and helping others.`;
 
 export const skills = [
-  "Entity Framework",
   "C#",
-  // Add more skills here
+  ".NET Core",
+  "Entity Framework",
+  "SQL Databases (Microsoft SQL Server, MySQL, PostgreSQL)",
+  "NoSQL Databases (MongoDB)",
+  "React.js",
+  "JavaScript",
+  "TypeScript",
+  "HTML & CSS (Fluent UI, Bootstrap)",
+  "Java",
+  "Cobol",
+  "Powershell",
+  "Python",
+  "Test Driven Development (TDD)",
+  "Backend Unit Tests (NUnit, Moq, MSTest, xUnit)",
+  "Frontend Unit Tests (Jest, Mocha)",
+  "Git & Various other Source Control Services",
+  "DevOps (Azure DevOps, Docker, Kubernetes)",
+  "IDEs (VS Code, Visual Studio, IntelliJ, Eclipse)",
+  "Scrum & Agile Development",
+  "Project Management (Azure DevOps, Jira)",
+  "Deployments, Continuous Integration and Continuous Delivery (CI/CD)",
+  "Application Programming Interfaces (API) Development",
+  "Microsoft Azure Services (Azure Functions, Azure SQL Database, Azure Service Fabric, Azure Active Directory, Azure Kubernetes Service, Azure Logic Apps, Azure Blob Storage, Azure Virtual Machines, Azure App Service, Azure Service Bus, Static Web Apps)",
+  "Solution Architecture",
+  "Computer Hardware Troubleshooting",
+  "Troubleshooting",
+  "Microsoft Office Suite (Word, Excel, PowerPoint, Teams, Presentation)",
 ];
 
 export const experiences = [
@@ -18,20 +44,107 @@ export const experiences = [
     role: "Software Engineer II",
     company: "Medical Solutions",
     duration: "Mar 2023 - Mar 2024",
+    skills: [
+      "C#",
+      ".NET Core",
+      "SQL",
+      "Azure",
+      "React",
+      "JavaScript",
+      "TypeScript",
+      "Cloud Platforms (Azure: Service Bus, App Configs, Blob Storage, etc.)",
+    ],
     overview:
-      "Project Overview: Contributed to the development and maintenance...",
+      "Contributed to the development and maintenance of a crucial system designed for processing and converting hospital timesheets into formats compatible with internal systems. This initiative ensured seamless data integration and enhanced operational efficiency.",
     contributions: [
-      "Collaborative Backend Overhaul...",
-      // Add more contributions as needed
+      "Collaborative Backend Overhaul: Actively participated in the collaborative effort to reimagine the backend architecture, focusing on scalability and future-proofing. Completely new V2 version of the code base, introducing 1000+ new tests for unit and integration.",
+      "Frontend Revitalization: Led the overhaul of the frontend to align with the new backend architecture.",
+      "Production Support and Data Management: Engaged in vital production support tasks.",
+      "Documentation and Knowledge Sharing: Authored detailed documentation for project repositories. Used by all other developers for reference of a starting point.",
+      "Team Dynamic and Culture Contribution: Excelled in fostering the BEST team environment possible.",
+      "Achievements: Played a central role in the successful revitalization of a codebase untouched for two years.",
     ],
   },
-  // Add more experiences here
+  {
+    role: "Software Engineer II",
+    company: "DLL",
+    duration: "Sep 2020 - Nov 2022",
+    skills: [
+      "React JS",
+      "JavaScript",
+      "Node",
+      "C# API",
+      "Azure SQL Database",
+      "AWS to Azure Migration",
+    ],
+    overview:
+      "Filled in for absent Scrum Master, overseeing 2 teams, and 4 projects. Mentored 2 interns, contributing to their development and integration within the team. Led a project on contracts tracking with a tech stack of React JS/JavaScript/Node with C# API backend using Azure SQL Database. Managed and enhanced the Express Finance Portal project, migrating from AWS to Azure with significant data migration. Supported the development of a customer-initiated credit application generating substantial revenue.",
+    contributions: [
+      "Scrum Master Leadership: Stepped in as Scrum Master, overseeing team dynamics and project management for 2 teams across 4 major projects.",
+      "Mentorship: Guided 2 interns through their development journey, integrating them into the team.",
+      "Technical Leadership: Led the development of a contract tracking project, showcasing strong skills in full-stack development.",
+      "Platform Migration: Successfully managed the migration of the Express Finance Portal from AWS to Azure, overseeing significant data migration challenges.",
+      "Innovation in Product Development: Contributed to the development of a customer-initiated credit application, directly impacting revenue growth.",
+    ],
+  },
+  {
+    role: "Developer II",
+    company: "Vermeer Corporation",
+    duration: "Oct 2019 - Sep 2020",
+    skills: [
+      "C#",
+      "WPF",
+      "Agile Development",
+      "Scrum Master",
+      "Application Architecture",
+    ],
+    overview:
+      "Led Agile Development as Scrum Master/Developer Combo. Developed the full architecture of a C#/WPF application, managed upgrades to existing applications, and redesigned a web application to enhance user satisfaction.",
+    contributions: [
+      "Agile Leadership: Served as both Scrum Master and Developer, facilitating agile development practices and team collaboration.",
+      "Application Development: Architected and developed a C#/WPF application, contributing significantly to the project's success.",
+      "Continuous Improvement: Managed upgrades to existing applications and led the redesign of a web application to improve user experience.",
+    ],
+  },
+  {
+    role: "Developer I",
+    company: "Vermeer Corporation",
+    duration: "Feb 2018 - Oct 2019",
+    skills: [
+      "WPF",
+      "C#",
+      ".NetCore",
+      "ASP.net - Razor",
+      "Database Management",
+      "Scrum and Agile Practices",
+    ],
+    overview:
+      "Utilized WPF, C#, .NetCore, ASP.net - Razor for enterprise applications. Managed large databases and integrated 3rd party software to improve efficiency. Adopted scrum and agile practices, developed workflow for new team structures, and acted as Scrum Master.",
+    contributions: [
+      "Enterprise Application Development: Leveraged WPF, C#, and .NetCore to develop and enhance enterprise-level applications.",
+      "Database and Integration: Managed large-scale databases and integrated 3rd party software, improving system efficiency and performance.",
+      "Agile and Scrum Implementation: Drove the adoption of scrum and agile practices within the team, enhancing project delivery and team dynamics.",
+    ],
+  },
+  {
+    role: "Software Developer Internship",
+    company: "Grinnell Mutual",
+    duration: "May 2017 - Aug 2017",
+    skills: ["Java", "Cobol", "HTML", "JavaScript"],
+    overview:
+      "Demonstrated skills in a professional work environment, working efficiently within a team and utilizing Java/Cobol/HTML/JavaScript in projects.",
+    contributions: [
+      "Team Collaboration: Worked effectively within a team, contributing to various projects.",
+      "Technology Proficiency: Utilized a diverse technology stack including Java, Cobol, HTML, and JavaScript in development projects.",
+    ],
+  },
 ];
 
 export const education = [
   {
-    school: "Indian Hills Community College",
-    degree: "Associate's degree, Computer Science",
+    school: "Indian Hills Community College 4.0 GPA",
+    degree:
+      "Associate Degree, Computer Science: Majored in Mainframe, Software Development, Web Development",
     duration: "2016 - 2018",
   },
   // Add more education entries if needed
